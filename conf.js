f827470a7b82b26ca370872c908dd4aa60d3bf18
Ludmila Omelchenko <omilka12@gmail.com>
2018-08-04 00:50:18 +0300

    updated to run not on local

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+   // seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
       'browserName': 'chrome',
       },
